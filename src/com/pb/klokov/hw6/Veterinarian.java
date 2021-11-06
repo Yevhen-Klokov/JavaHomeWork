f5a09e9a60e58250ff5842e31699d8fc74874ce7
Yevhen Klokov <dp290790kem@gmail.com>
2021-11-06 20:48:49 +0200

    init commit

--- src/com/pb/klokov/hw6/Veterinarian.java
+++ src/com/pb/klokov/hw6/Veterinarian.java
@@ -1,10 +1,11 @@
 package com.pb.klokov.hw6;
 
 public class Veterinarian {
-    public void treatAnimal(Animal animal){
+    public static void treatAnimal(Animal animal){
         System.out.println("У ветеренара на приёме животное: "+ animal.getAnimal() + ", еда - " + animal.getFood() + ", живёт - " + animal.getLocation());
-
     }
 }
+
+
 
 
--- src/com/pb/klokov/hw6/VetСlinic.java
+++ src/com/pb/klokov/hw6/VetСlinic.java
@@ -1,7 +1,9 @@
 package com.pb.klokov.hw6;
 
+import java.lang.reflect.Constructor;
+
 public class VetСlinic {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
     Animal dog = new Dog();
     Animal cat = new Cat();
     Animal horse = new Horse();
@@ -11,10 +13,21 @@ public class VetСlinic {
 //        for (int i = 0; i < animals.length; i++) {
 //            System.out.print(animals[i]);
 //        }
-    Veterinarian vet = new Veterinarian();
-        System.out.println("Отправляем животных к ветеренару:");
-        for (int i = 0; i < animals.length; i++) {
-            vet.treatAnimal(animals[i]);
+//    Veterinarian vet = new Veterinarian();
+//        System.out.println("Отправляем животных к ветеринару:");
+//        for (int i = 0; i < animals.length; i++) {
+//            vet.treatAnimal(animals[i]);
+//        }
+//        System.out.println(Veterinarian.class.getName());
+//        com.pb.klokov.hw6.Veterinarian
+
+    Class clazz = Class.forName("com.pb.klokov.hw6.Veterinarian");
+        Constructor constr = clazz.getConstructor();
+        Object obj = constr.newInstance();
+        if(obj instanceof Veterinarian){
+            for (int i = 0; i < animals.length; i++) {
+                Veterinarian.treatAnimal(animals[i]);
+            }
         }
     }
 
