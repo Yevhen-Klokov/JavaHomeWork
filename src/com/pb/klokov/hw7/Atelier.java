74287077b8afabbd3bf12e77c744cf7727c6fbae
Yevhen Klokov <dp290790kem@gmail.com>
2021-11-15 19:10:07 +0200

    init commit

--- src/com/pb/klokov/hw7/Atelier.java
+++ src/com/pb/klokov/hw7/Atelier.java
@@ -1,50 +1,35 @@
 package com.pb.klokov.hw7;
 
 public class Atelier {
-    public static void main(String[] args) throws Exception {
+
+        public static void main(String[] args) throws Exception {
         Clothes tshirt = new Tshirt(Size.L, 1000, "green");
         Clothes pants = new Pants(Size.M, 2000, "black");
         Clothes skirt = new Skirt(Size.S,1200, "red");
         Clothes tie = new Tie(Size.XS,500,"yellow");
 
         Clothes[] clothes = {tshirt,pants,skirt,tie};
+        dressMan(clothes);
+        dressWomen(clothes);
 
-//        ((Tshirt) tshirt).dressMan();
-//        ((Pants) pants).dressMan();
+    }
+    static void dressMan (Clothes[] clothes) {
         System.out.println("Мужская одежда:");
-        for(Clothes clothes1 : clothes){
-            if(clothes1 instanceof Tshirt){
-                ((Tshirt) tshirt).dressMan();
+        for (Clothes clothe : clothes) {
+            if (clothe instanceof ManClothes) {
+                ((ManClothes) clothe).dressMan();
             }
-            else if(clothes1 instanceof Pants){
-                ((Pants) pants).dressMan();
-            }
-            else if(clothes1 instanceof Tie){
-                ((Tie) tie).dressMan();
-            }
-
         }
-        System.out.println("");
+    }
 
+    static void dressWomen (Clothes[] clothes) {
         System.out.println("Женская одежда:");
-        for(Clothes clothes1 : clothes){
-            if(clothes1 instanceof Tshirt){
-                ((Tshirt) tshirt).dressWomen();
+        for (Clothes clothe : clothes) {
+            if (clothe instanceof WomenClothes) {
+                ((WomenClothes) clothe).dressWomen();
             }
-            else if(clothes1 instanceof Pants){
-                ((Pants) pants).dressWomen();
-            }
-            else if(clothes1 instanceof Skirt){
-                ((Skirt) skirt).dressWomen();
-            }
-
         }
-
     }
 
-    public static void dressMan(Clothes[] clothes){
-    };
-    public static void dressWomen(Clothes[] clothes){
-    }
 
 }
