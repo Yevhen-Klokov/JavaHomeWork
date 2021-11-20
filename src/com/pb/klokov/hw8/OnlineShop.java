59d5d8747cb48a78f1c7cde5091fe96a2614bba5
Yevhen Klokov <dp290790kem@gmail.com>
2021-11-20 20:58:41 +0200

    init commit

--- src/com/pb/klokov/hw8/OnlineShop.java
+++ src/com/pb/klokov/hw8/OnlineShop.java
@@ -5,14 +5,12 @@ import java.util.Scanner;
 public class OnlineShop {
     public static void main(String[] args) {
         System.out.print("Регистрация в OnlineShop! \n Введите имя: ");
-        Scanner scan1 = new Scanner(System.in);
-        String setLogin = scan1.next();
+        Scanner scan = new Scanner(System.in);
+        String setLogin = scan.nextLine();
         System.out.print("Введите пароль: ");
-        Scanner scan2 = new Scanner(System.in);
-        String setPassword = scan2.next();
+        String setPassword = scan.nextLine();
         System.out.print("Подтвердите пароль: ");
-        Scanner scan3 = new Scanner(System.in);
-        String setPassword2 = scan3.next();
+        String setPassword2 = scan.nextLine();
 
         //создаём объект класса
         Auth auth = new Auth(setLogin,setPassword,setPassword2);
