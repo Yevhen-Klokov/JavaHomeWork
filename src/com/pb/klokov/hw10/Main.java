fbd20d87303e5ac953a5ec709315485d129c8469
Yevhen Klokov <dp290790kem@gmail.com>
2021-11-27 15:04:57 +0200

    init commit

--- src/com/pb/klokov/hw10/Main.java
+++ src/com/pb/klokov/hw10/Main.java
@@ -1,16 +1,24 @@
 package com.pb.klokov.hw10;
 
+import com.pb.klokov.hw3.Array;
+
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         NumBox<Number> nums = new NumBox<>(5);
-        nums.set(0, new Number(1));
-        nums.set(1, new Number(2));
-        nums.set(2, new Number(3));
-        nums.set(3, new Number(4));
-        nums.set(4, new Number(5));
+        try {nums.add(0, new Number(1));
+            nums.add(1, new Number(2));
+            nums.add(2, new Number(3));
+            nums.add(3, new Number(4));
+            nums.add(4, new Number(5));
+        } catch (Exception e) {
+            System.out.println("Не возможно добавить элемент т.к. массив переполнен!");
+        }
 
-        Number n1 = nums.get(0);
-        System.out.println(n1.getNumber());
-
+        System.out.println("К-во элементов массива " + nums.length());
+        System.out.println("Среднее значение элементов массива " + nums.average());
+        System.out.println("Сумма элеменов массива " + nums.sum());
+        System.out.println("Максимальний элемент массива " + nums.max());
     }
 }
--- src/com/pb/klokov/hw10/NumBox.java
+++ src/com/pb/klokov/hw10/NumBox.java
@@ -1,33 +1,58 @@
 package com.pb.klokov.hw10;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class NumBox<T extends Number> {
 
-private final T[] numbers;
-@SuppressWarnings("unchecked")
+    private final T[] numbers;
+
+    @SuppressWarnings("unchecked")
 
     public NumBox(int size) {
         numbers = (T[]) new Number[size];
-}
-    public T get(int index){
+    }
+
+    public T get(int index) {
         return numbers[index];
     }
-    public void set(int index, T number){
-    this.numbers[index] = number;
+
+    public void add(int index, T number) {
+        this.numbers[index] = number;
     }
 
-    public void add(T num){
-        for(int i = 0; i < numbers.length; i++){
-            Random random = new Random();
-            try {
-                numbers[i] = get(i);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+    public int length() {
+        int count = 0;
+        for(T t : numbers){
+            count++;
         }
+        return count;
+    }
+
+    public double sum (){
+        double sum = 0;
+        for (T t : numbers){
+            sum += t.number;
+        }
+        return sum;
+    }
+
+    public double average (){
+        double sum = 0;
+        for (T t : numbers){
+            sum += t.number;
+        }
+        return sum / numbers.length;
+    }
+
+//    public T max (){
+//        Arrays.stream(numbers).sorted();
+//        return numbers[0];
+//    }
+
 
     }
-}
+
+
 
 
